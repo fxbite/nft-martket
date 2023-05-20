442c7fcfbf44461b70785fa0ccd242e0c6e9ef92
fxbite <letruongthinh1703@gmail.com>
2023-05-20 19:03:56 +0000

    Add API Key infura

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,8 +1,21 @@
 import { useState } from 'react'
 import { ethers } from "ethers"
 import { Row, Form, Button } from 'react-bootstrap'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+import {Buffer} from 'buffer';
+import {create} from 'ipfs-http-client'
+const projectId = '2Q4NjjPQ0AhrS6vl8MUNaoWAmNz';
+const projectSecret = 'd20fd4497d5e302c40f89d195497cb11';
+
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+const client = create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  apiPath: '/api/v0',
+  headers: {
+    authorization: auth,
+  }
+})
 
 const Create = ({ marketplace, nft }) => {
   const [image, setImage] = useState('')
